37c243a2ee5d06613ffd61fffd07982d8d46917b
whl <1023680139@qq.com>
2021-02-15 23:10:32 +0800

    feat:添加生成位置的属性

--- assets/script/util.js
+++ assets/script/util.js
@@ -5,14 +5,16 @@ module.exports = {
      * fix：未注明类型，待补全
      * 
      * 返回一个存储障碍物属性的对象
+     * @param {cc.Vec2} startPos 生成位置
      * @param {*} speed 移动速度
      * @param {*} amplitude 振幅，仅对ufo有效
      * @param {*} angularVelocity 角速度，仅对ufo有效
      * @param {*} phase 初相，仅对ufo有效
      */
-    moveLogicParam (speed = 1000, amplitude = 100, angularVelocity = 0.001, phase = 0){
+    moveLogicParam (startPos =cc.v2(-500, 500) ,speed = 1000, amplitude = 100, angularVelocity = 0.001, phase = 0){
 
         return {
+            startPos,
             speed,
             amplitude,
             angularVelocity,
